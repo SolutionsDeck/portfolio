b84f5db450d67b2fd34971cb3f51e76b7268de8b
Amogh Ak <amoghak143@gmail.com>
2023-07-29 00:02:47 -0700

    >>>

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,11 +25,11 @@ const Contact = () => {
             <i className="bx bx-share-alt"></i>
             <h3>Social Profiles</h3>
             <div className="social-links">
-              <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
-              <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
-              <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
-              <a href="#" className="google-plus"><i className="bi bi-skype"></i></a>
-              <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
+              <a href="#!" className="twitter"><i className="bi bi-twitter"></i></a>
+              <a href="#!" className="facebook"><i className="bi bi-facebook"></i></a>
+              <a href="#!" className="instagram"><i className="bi bi-instagram"></i></a>
+              <a href="#!" className="google-plus"><i className="bi bi-skype"></i></a>
+              <a href="#!" className="linkedin"><i className="bi bi-linkedin"></i></a>
             </div>
           </div>
         </div>
@@ -50,7 +50,7 @@ const Contact = () => {
         </div>
       </div>
 
-      <form action="forms/contact.php" method="post" role="form" className="php-email-form mt-4">
+      <form method="post" role="form" className="php-email-form mt-4">
         <div className="row">
           <div className="col-md-6 form-group">
             <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
--- src/components/header.js
+++ src/components/header.js
@@ -23,10 +23,10 @@ const Header = () => {
       </nav>
 
       <div className="social-links">
-        <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
-        <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
-        <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
-        <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
+        <a href="#!" className="twitter"><i className="bi bi-twitter"></i></a>
+        <a href="#!" className="facebook"><i className="bi bi-facebook"></i></a>
+        <a href="#!" className="instagram"><i className="bi bi-instagram"></i></a>
+        <a href="#!" className="linkedin"><i className="bi bi-linkedin"></i></a>
       </div>
 
     </div>
